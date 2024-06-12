0f99307ab54438e674df481743e6b7d9114941c8
Yonatan Tal <yonatantal@MacBook-14.local>
2024-06-12 11:33:00 +0300

    changed app.py for final testing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ website = Flask("__name__")
 # when user browse into '/', index function is called
 @website.route('/')
 def index():
-    return "<h1>Hello World</h1>"
+    return '<h1 style="color:MediumSeaGreen; font-family:verdana;">Hello World!</h1>'
 
 
-website.run(host="0.0.0.0", port=80)
+# website.run(host="0.0.0.0", port=1234)
